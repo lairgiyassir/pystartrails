6e64a872850a55e32105ec497aa94ac11cdc98ba
Yassir LAIRGI <yassirlairgi@gmail.com>
2025-07-13 23:45:35 +0200

    Refactor TrailsGenerator import and add TrailsAndTimeLapseGenerator class for star trails and time-lapse video generation

--- pystartrails/generator/__init__.py
+++ pystartrails/generator/__init__.py
@@ -1,2 +1,3 @@
-from .trailsgenerator import TrailsGenerator
-from .trailsgenerator import TrailsAndTimeLapseGenerator
+from .trails_and_timelapse_generator import TrailsAndTimeLapseGenerator
+
+__all__ = ['TrailsAndTimeLapseGenerator']
